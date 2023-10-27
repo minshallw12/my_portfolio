20f53cac33dc5a41a6f62ad2728cca6ff5befb91
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-27 16:41:51 -0600

    played with responsive design

--- portfolio_project/frontend/src/components/header/Navbar.jsx
+++ portfolio_project/frontend/src/components/header/Navbar.jsx
@@ -5,16 +5,19 @@ import { Link } from 'react-router-dom';
 
 export default function MyNav() {
   return (
-    <Navbar bg="white" expand="lg" style={{background:'white'}}>
-      <Container style={{display:'flex', alignItems:'center', justifyContent:'center', height: '30px'}}>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto navcenter center" style={{display:'flex', justifyContent:'space-around', width: "900px"}}>
-            <Nav.Link ><Link className = 'NavLinks' to='/'>Home</Link></Nav.Link>
-            <Nav.Link ><Link className = 'NavLinks' to='/aboutme'>About me</Link></Nav.Link>
-            <Nav.Link ><Link className = 'NavLinks' to='/projects'>My Projects</Link></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar className="navbar"bg="white" expand="lg">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto navcenter center" id='navbar-children'>
+              <Nav.Link ><Link className = 'NavLinks' to='/'>Home</Link></Nav.Link>
+              <Nav.Link ><Link className = 'NavLinks' to='/aboutme'>About me</Link></Nav.Link>
+              <Nav.Link ><Link className = 'NavLinks' to='/projects'>My Projects</Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+    
   );
 }
